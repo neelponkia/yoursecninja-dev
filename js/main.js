b38e92cb99e48323fa38cae409926d85d0b19756
Neel1323 <Neel1323@users.noreply.github.com>
2018-08-04 21:04:53 +0530

    Products Page

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ $(document).ready( function() {
 	    margin:80, 
 	    items: 5,
         loop: true,
-        autoplayTimeout: 2000,
+        autoplayTimeout: 2500,
         autoplayHoverPause: true,
         autoplay: true,
         responsive: {
---
+++ js/products-main.js
@@ -0,0 +1,10 @@
+$(document).ready( function() {
+    "use strict";
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('header-scrolled');
+        } else {
+            $('#header').removeClass('header-scrolled');
+        }
+    });
+});
